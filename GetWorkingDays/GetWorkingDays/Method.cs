84e261dc06d06650ecda7c583802bd8b5ff495ca
jwyanh <83310997+jwyanh@users.noreply.github.com>
2022-02-03 09:09:02 -0800

    Add files via upload

---
+++ GetWorkingDays/GetWorkingDays/Method.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+
+namespace ConvertDate_Jiewen
+{
+	public class Method
+	{
+		public string[] InputDate()
+		{
+			Console.WriteLine("Input first Date:");
+			string[] Num = new string[2];
+
+			Num[0] = Console.ReadLine();
+
+			Console.WriteLine("Input Second Date:");
+			Num[1] = Console.ReadLine();
+
+			return Num;
+		}
+
+
+	}
+}
---
+++ GetWorkingDays/GetWorkingDays/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConvertDate_Jiewen
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Method methodExample = new Method();
+            
+            string[] Num = methodExample.InputDate();
+
+            DateTime dateTime1 = DateTime.Parse(Num[0]);
+            DateTime dateTime2 = DateTime.Parse(Num[1]);
+            TimeSpan diff1 = dateTime2.Subtract(dateTime1);
+
+            //Tring to implement DateTimeExtension To get the working days directly
+            //Otherwise, not sure the clear idea of get the holliday since it only specifies the date for a single year
+
+            Console.WriteLine(diff1.ToString("%d"));
+
+        }
+    }
+}
