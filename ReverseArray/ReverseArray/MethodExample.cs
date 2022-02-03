aa436e943080418193b832653ffd5eb09cf4f4e0
jwyanh <83310997+jwyanh@users.noreply.github.com>
2022-02-03 09:07:53 -0800

    Add files via upload

---
+++ ReverseArray/ReverseArray/MethodExample.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace ReverseArray
+{
+	class MethodExample
+	{
+		public int[] GenerateNumbers()
+		{
+			string Num = Console.ReadLine();
+			int Input = Int16.Parse(Num);
+
+			int[] Result = new int[Input];
+			for (int i = 0; i < Result.Length; i++)
+				Result[i] = i+1;
+
+			return Result;
+		}
+
+		public void PrintNumbersmethod(int[] numbers)
+        {
+			foreach (var entry in numbers)
+			{
+				Console.Write(entry + " ");
+			}
+			Console.Write("\n");
+		}
+
+		public int[] Reverse(int[] array)
+        {
+			for (int i = 0; i <= array.Length / 2; i++)
+			{
+				int temp = array[i];
+				array[i] = array[array.Length - i -1];
+				array[array.Length - i-1] = temp;
+
+			}
+			return array;
+
+		}
+
+		public int Fibonacci(int Input)
+        {
+			if (Input == 1 || Input == 2)
+            {
+				return 1;
+            }
+
+			return Fibonacci(Input - 1) + Fibonacci(Input - 2);
+
+		}
+	}
+}
+
---
+++ ReverseArray/ReverseArray/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ReverseArray 
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            MethodExample methodExample = new MethodExample();
+
+            int[] numbers = methodExample.GenerateNumbers();
+
+            //Printing array
+            methodExample.PrintNumbersmethod(numbers);
+
+            int[] ReverseNumber = methodExample.Reverse(numbers);
+            methodExample.PrintNumbersmethod(ReverseNumber);
+
+
+            //Fibonacci
+            int input = 10;
+            int[] Result = new int[input];
+
+            for (int i = 0; i < Result.Length; i++)
+                Result[i] = methodExample.Fibonacci(i+1);
+
+            methodExample.PrintNumbersmethod(Result);
+
+            
+        }
+    }
+}
